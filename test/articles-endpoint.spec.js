2911835a2c41e2c185c9e355f945004f82f61d99
nbeers22 <nbeers22@gmail.com>
2019-06-28 15:15:50 -0400

    Set up initial test for GET /bookmarks route

--- test/articles-endpoint.spec.js
+++ test/articles-endpoint.spec.js
@@ -1,4 +1,50 @@
-const knex = require('knex')
-const app = require('../src/app')
+const knex = require('knex');
+const app = require('../src/app.js');
 const testBookmarks = require('./bookmarks.fixtures.js');
 
+describe.only('Bookmarks Endpoint', () => {
+  let db;
+  const headers = {
+    Authorization: 'Bearer abcd12345'
+  }
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL
+    });
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => (
+    db.destroy()
+  ));
+
+  before('clean the table', () => (
+    db('bookmarks').truncate()
+  ));
+
+  afterEach('Remove test bookmarks from database', () => (
+    db('bookmarks').truncate()
+  ));
+
+  describe('GET /bookmarks', () => {
+    
+    context('Given bookmarks table has data', () => {
+      
+      beforeEach('Insert test bookmarks into the db', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      });
+
+      it('responds with 200 and list of all bookmarks in db', () => {
+        return supertest(app)
+          .get('/bookmarks')
+          .set(headers)    
+          .expect(200,testBookmarks)
+      });
+    });
+  });
+  
+});
--- test/bookmarks.fixtures.js
+++ test/bookmarks.fixtures.js
@@ -1,17 +1,20 @@
 const testBookmarks = [
   {
+    id: 1,
     title: 'First bookmark',
     url: 'https://nathanbeers.com',
     description: 'A cool site',
     rating: 4
   },
   {
+    id: 2,
     title: 'Second bookmark',
     url: 'https://theverge.com',
     description: 'A really cool blog',
     rating: 3
   },
   {
+    id: 3,
     title: 'First bookmark',
     url: 'https://css-tricks.com',
     description: 'A cool site for devs',
