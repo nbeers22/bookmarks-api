04876ca9ae71cc0b6ba9013057f561d0bf258ccb
nbeers22 <nbeers22@gmail.com>
2019-06-28 10:20:03 -0400

    Completing setup of postgrator and migration/seed files

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,9 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+  ('First Bookmark', 'https://google.com', 'A search engine', 3),
+  ('Second Bookmark', 'https://facebook.com', 'A social media site', 2),
+  ('Third Bookmark', 'https://foxnews.com', 'A media site', 1),
+  ('Fourth Bookmark', 'https://cnn.com', 'A media site', 1),
+  ('Fifth Bookmark', 'https://twitter.com', 'A social media site', 3),
+  ('Sixth Bookmark', 'https://steam.com', 'A gaming site', 4)
+;
