ea0b26be0930a7e03ac43d90d829368baec353eb
nbeers22 <nbeers22@gmail.com>
2019-06-28 10:44:10 -0400

    Updated db and test db string

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 4000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'abcd12345',
+  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
+  TEST_DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks-test'
 }
