1fd4369b9ff372cb20271310e688e1bd04f9e11d
nbeers22 <nbeers22@gmail.com>
2019-06-28 13:54:17 -0400

    Created test bookmarks file for testing purposes only

---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,22 @@
+const testBookmarks = [
+  {
+    title: 'First bookmark',
+    url: 'https://nathanbeers.com',
+    description: 'A cool site',
+    rating: 4
+  },
+  {
+    title: 'Second bookmark',
+    url: 'https://theverge.com',
+    description: 'A really cool blog',
+    rating: 3
+  },
+  {
+    title: 'First bookmark',
+    url: 'https://css-tricks.com',
+    description: 'A cool site for devs',
+    rating: 5
+  },
+]
+
+module.exports = testBookmarks;
