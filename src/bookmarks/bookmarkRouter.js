d15600ac684b4ee1a13eed53feb241edb79662c6
nbeers22 <nbeers22@gmail.com>
2019-06-29 08:40:15 -0400

    Refactored DELETE /bookmarks/:id to use BookmarkService function

--- src/bookmarks/bookmarkRouter.js
+++ src/bookmarks/bookmarkRouter.js
@@ -59,16 +59,17 @@ bookmarkRouter
   })
   .delete( (req,res) => {
     const { id } = req.params;
-    const bookmarkIndex = bookmarks.findIndex( bookmark => (
-      bookmark.id === +id
-    ));
-    // if bookmark isn't found
-    if (bookmarkIndex === -1) {
-      logger.error(`Bookmark not found at ${req.path}`);
-      return res.status(404).json({error: "Bookmark not found"});
-    }
-    bookmarks.splice(bookmarkIndex,1);
-    res.status(200).json({success: `Bookmark ${id} Deleted`});
+    const knexInstance = req.app.get('db');
+    
+    BookmarksService.deleteBookmark(knexInstance,id)
+      .then(bookmark => {
+        if(!bookmark){
+          logger.error(`Bookmark not found at ${req.path}`);
+          return res.status(404).json({error: "Bookmark not found"});
+        }
+        res.status(200).json({success: `Bookmark ${id} Deleted`});
+      })
+    
   });
 
 module.exports = bookmarkRouter;
